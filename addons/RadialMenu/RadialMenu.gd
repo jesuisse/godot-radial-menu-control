0709e70f53cb289f2d529b6f6d2622fa77b973ff
Nick Royer <94314449+nwroyer@users.noreply.github.com>
2022-05-18 12:44:29 -0700

    Fixed bug when setting submenu items in script
    Added a check to address the case where we're setting items on a RadialMenu that's not in the scene tree yet; this allows submenus to be built dynamically in another script.

--- addons/RadialMenu/RadialMenu.gd
+++ addons/RadialMenu/RadialMenu.gd
@@ -580,9 +580,10 @@ func _get_constant(name):
 
 func _clear_items():
 	var n = $ItemIcons
-	for node in n.get_children():
-		n.remove_child(node)	
-		node.queue_free()
+	if n != null:
+		for node in n.get_children():
+			n.remove_child(node)	
+			node.queue_free()
 	
 
 func get_itemindex_from_vector(v: Vector2):
