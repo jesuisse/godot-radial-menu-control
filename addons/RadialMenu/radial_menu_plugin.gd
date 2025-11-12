f598801c534603d50a90c2a845619d7b529f6fbe
Pascal Schuppli <pschuppli@phar.ch>
2025-11-12 01:26:03 +0100

    Fixed issue #10: Plugin error caused by old 'Popup' instead of 'Control' node.

--- addons/RadialMenu/radial_menu_plugin.gd
+++ addons/RadialMenu/radial_menu_plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("RadialMenu", "Popup", preload("RadialMenu.gd"), preload("icons/radial_menu.svg"))
+	add_custom_type("RadialMenu", "Control", preload("RadialMenu.gd"), preload("icons/radial_menu.svg"))
 	
 func _exit_tree():
 	remove_custom_type("RadialMenu")
